2bdd8005a9bfc510dec78f069ba03bc9f9616e4f
Željko Filipin <zeljko.filipin@gmail.com>
2022-09-27 12:08:00 +0200

    data: get the size of the smallest table

--- test/specs/data.js
+++ test/specs/data.js
@@ -1,14 +1,11 @@
 'use strict';
-describe( 'My Login application', () => {
-	it( 'should login with valid credentials', async () => {
-		await browser.url( 'https://the-internet.herokuapp.com/login' );
-
-		await $( '#username' ).setValue( 'tomsmith' );
-		await $( '#password' ).setValue( 'SuperSecretPassword!' );
-		await $( 'button[type="submit"]' ).click();
-
-		await expect( $( '#flash' ) ).toBeExisting();
-		await expect( $( '#flash' ) ).toHaveTextContaining(
-			'You logged into a secure area!' );
+describe( 'Developers/Maintainers', () => {
+	it( 'should output data', async () => {
+		await browser.url( '' );
+		// await browser.debug();
+		const size = ( await $( 'table.wikitable:nth-child(52)' ).$$( 'tr' ).length - 1 );
+		console.log( '----------' );
+		console.log( size );
+		console.log( '----------' );
 	} );
 } );
