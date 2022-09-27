fac447a363955ca7e786d7ffefa6ad2dd3b515ce
Željko Filipin <zeljko.filipin@gmail.com>
2022-09-27 14:33:15 +0200

    data: core and extensions

--- test/specs/data.js
+++ test/specs/data.js
@@ -1,11 +1,29 @@
 'use strict';
+
 describe( 'Developers/Maintainers', () => {
 	it( 'should output data', async () => {
 		await browser.url( '' );
-		// await browser.debug();
-		const size = ( await $( 'table.wikitable:nth-child(52)' ).$$( 'tr' ).length - 1 );
-		console.log( '----------' );
-		console.log( size );
-		console.log( '----------' );
+
+		// core
+		const coreComponents = ( await $( 'table.wikitable:nth-child(17)' ).$$( 'tr' ).length - 1 );
+		const coreUnassigned = await $( 'table.wikitable:nth-child(17)' ).$$( 'td=Unassigned' ).length;
+		const corePercentage = ( coreUnassigned / coreComponents * 100 ).toFixed( 0 );
+
+		// extensions
+		const extensionsComponents = ( await $( 'table.wikitable:nth-child(22)' ).$$( 'tr' ).length - 1 );
+		const extensionsUnassigned = await $( 'table.wikitable:nth-child(22)' ).$$( 'td=Unassigned' ).length;
+		const extensionsPercentage = ( extensionsUnassigned / extensionsComponents * 100 ).toFixed( 0 );
+
+		console.log( '------------------------------------------------------------------------------------------------------------------------' );
+		console.log( 'Core' );
+		console.log( '- Components: ' + coreComponents );
+		console.log( '- Unassigned: ' + coreUnassigned );
+		console.log( '-          %: ' + corePercentage );
+		console.log( '------------------------------------------------------------------------------------------------------------------------' );
+		console.log( 'Extensions' );
+		console.log( '- Components: ' + extensionsComponents );
+		console.log( '- Unassigned: ' + extensionsUnassigned );
+		console.log( '-          %: ' + extensionsPercentage );
+		console.log( '------------------------------------------------------------------------------------------------------------------------' );
 	} );
 } );
