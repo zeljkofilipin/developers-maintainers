598643b6b464081907223417efde5cac275f45ef
Željko Filipin <zeljko.filipin@gmail.com>
2022-09-27 14:43:25 +0200

    data: extract functionality to a function

--- test/specs/data.js
+++ test/specs/data.js
@@ -1,29 +1,22 @@
 'use strict';
 
+async function table( name, int ) {
+	const selector = `table.wikitable:nth-child(${int})`;
+	const components = ( await $( selector ).$$( 'tr' ).length - 1 );
+	const unassigned = await $( selector ).$$( 'td=Unassigned' ).length;
+	const percentage = ( unassigned / components * 100 ).toFixed( 0 );
+	console.log( '------------------------------------------------------------------------------------------------------------------------' );
+	console.log( name );
+	console.log( '- Components: ' + components );
+	console.log( '- Unassigned: ' + unassigned );
+	console.log( '-          %: ' + percentage );
+	console.log( '------------------------------------------------------------------------------------------------------------------------' );
+}
+
 describe( 'Developers/Maintainers', () => {
 	it( 'should output data', async () => {
 		await browser.url( '' );
-
-		// core
-		const coreComponents = ( await $( 'table.wikitable:nth-child(17)' ).$$( 'tr' ).length - 1 );
-		const coreUnassigned = await $( 'table.wikitable:nth-child(17)' ).$$( 'td=Unassigned' ).length;
-		const corePercentage = ( coreUnassigned / coreComponents * 100 ).toFixed( 0 );
-
-		// extensions
-		const extensionsComponents = ( await $( 'table.wikitable:nth-child(22)' ).$$( 'tr' ).length - 1 );
-		const extensionsUnassigned = await $( 'table.wikitable:nth-child(22)' ).$$( 'td=Unassigned' ).length;
-		const extensionsPercentage = ( extensionsUnassigned / extensionsComponents * 100 ).toFixed( 0 );
-
-		console.log( '------------------------------------------------------------------------------------------------------------------------' );
-		console.log( 'Core' );
-		console.log( '- Components: ' + coreComponents );
-		console.log( '- Unassigned: ' + coreUnassigned );
-		console.log( '-          %: ' + corePercentage );
-		console.log( '------------------------------------------------------------------------------------------------------------------------' );
-		console.log( 'Extensions' );
-		console.log( '- Components: ' + extensionsComponents );
-		console.log( '- Unassigned: ' + extensionsUnassigned );
-		console.log( '-          %: ' + extensionsPercentage );
-		console.log( '------------------------------------------------------------------------------------------------------------------------' );
+		await table( 'Core', 17 );
+		await table( 'Extensions', 22 );
 	} );
 } );
